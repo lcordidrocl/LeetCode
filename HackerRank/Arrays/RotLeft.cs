3a5f174a5cfdcf52f8b4b8664573e04ee0961991
lucho.c <luchocordi@hotmail.com>
2021-07-17 23:42:36 -0300

    rotLeft

---
+++ HackerRank/Arrays/RotLeft.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackeRank.Arrays
+{
+    public static class RotLeft
+    {
+        public static List<int> rotLeft(List<int> a, int d)
+        {
+            var unchangedList = a.Skip(d);
+            var leftSortedList = unchangedList.Concat(a.Take(d)).ToList();
+            return leftSortedList;
+        }
+    }
+}
